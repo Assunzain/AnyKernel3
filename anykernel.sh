6a59d890c310c88d6a7a98e6edcbdc5953583ecc
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-07-20 19:49:21 +0700

    X01AD: Fix flashing no ramdisk

--- anykernel.sh
+++ anykernel.sh
@@ -4,24 +4,24 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=GreenLight Kernel
+kernel.string=Installing Kernel...
 maintainer.string1=AssunZain
-maintainer.string2=Nothing
+version.kernel=v2.3 KernelSU
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
-do.cleanuponabort=1
-device.name1=X01A
-device.name2=X01AD
-supported.versions=
+do.cleanuponabort=0
+device.name1=X01AD
+device.name2=X01A
+supported.versions=10 - 14
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=auto;
-ramdisk_compression=auto;
+is_slot_device=0;
+ramdisk_compression=lz4-l;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -35,25 +35,8 @@ set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
-## AnyKernel boot install
+## AnyKernel install
 dump_boot;
 
-
 write_boot;
-## end boot install
-
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
+## end install
