550ea380813e88725299ed089c2d2c18b39fbc1a
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-22 01:15:32 +0700

    X01AD: Merge KSU BETA v2.2

--- anykernel.sh
+++ anykernel.sh
@@ -6,7 +6,7 @@
 properties() { '
 kernel.string=Installing Kernel...
 maintainer.string1=AssunZain
-version.kernel=v2.1 OC KSU BETA
+version.kernel=v2.2 OC KSU BETA
 do.devicecheck=1
 do.modules=0
 do.systemless=1
